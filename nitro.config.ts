24577fcd109e906bb9d88433ec60eade5dfcd0e0
Martin Dimitrov <mdimitrov@sbtsolution.com>
2026-01-31 20:29:36 +0200

    update nitro

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,9 +2,9 @@
 import { defineNitroConfig } from 'nitropack'
 
 export default defineNitroConfig({
-  preset: 'netlify',
+  preset: 'vercel',
   output: {
-    dir: 'dist',
+    dir: '.output',
     // For Nitro v3 the serverDir is used to control where the server files live
     serverDir: 'server'
   }
