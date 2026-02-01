015e544c6ad344a02d56325ec59dbb6a531dd75c
Martin Dimitrov <mdimitrov@sbtsolution.com>
2026-02-01 12:50:01 +0200

    update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,7 +7,7 @@ export const Modal = ({
 }) => {
 	return (
 		<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
-			<div className="bg-gray-800 rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto relative">
+			<div className="bg-gray-800 rounded-lg max-w-2xl w-full pb-8 max-h-[90vh] overflow-y-auto relative">
 				<button
 					type="button"
 					onClick={onClose}
